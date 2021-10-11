ee3afe8796b50c921f87b5b747c76039a9221933
Ryan <ryangibson365@gmail.com>
2021-10-11 14:10:32 -0500

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  mode: 'jit',
+  purge: {
+    enabled: true,
+    content: ["src/*.html"],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'daily-dev-tips': "#F89283"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
